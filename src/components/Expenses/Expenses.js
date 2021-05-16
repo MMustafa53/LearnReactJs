d51ba551b8ddde65c128ef72eaea1501b0e55baa
Mustafa Husrevoglu <muhammed.husrevoglu@prodabilisim.com>
2021-05-16 16:21:37 +0300

    Filter added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+    const [year, setYear] = useState('2020')
+    const changeFilterYear = selectedYear => {
+        setYear(selectedYear)
+    }
     let items = (
-        props.items.map(item => {
+        props.items.filter(item => item.date.getFullYear().toString() === year).map(item => {
             return(
                 <ExpenseItem
                     key={item.id}
@@ -18,9 +23,12 @@ const Expenses = (props) => {
         })
     );
     return (
-        <Card className="expenses">
-            {items}
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter year={year} onYear={changeFilterYear}/>
+                {items}
+            </Card>
+        </div>
     );
 }
 
